013af7c9e7dcecd263c0da53c1395f5407ddbffe
PhiX <pokphoenix@gmail.com>
2021-12-15 16:12:33 +0700

    typescript support

---
+++ pages/news/index.js
@@ -0,0 +1,15 @@
+const News = ({data}) => {
+    return (  
+        <h1 className='content'>{data}</h1>
+    );
+}
+ 
+export default News;
+
+export async function getStaticProps(){
+    return {
+        props: {
+            data: 'List of published articles',
+        }
+    }
+}
